1165ae324ddc4fa52631a49058c7c38a5a81c0a4
fseneque <fseneque@ITEM-87030.dhcp.ntes.fr.sopra>
2016-07-06 00:52:35 +0200

    Initial Commit

---
+++ src/main/java/com/soprasteria/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.soprasteria.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.soprasteria.controller")
+public class AppConfig {
+
+}
---
+++ src/main/java/com/soprasteria/controller/AppController.java
@@ -0,0 +1,21 @@
+package com.soprasteria.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class AppController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String showIndex(Model model) {
+    	return "index";
+    }
+
+    @RequestMapping("/helloWorld")
+    public String helloWorld(Model model) {
+        model.addAttribute("message", "Hello World!");
+        return "helloWorld";
+    }
+}
